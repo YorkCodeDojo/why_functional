09ff5298ee00c0516ec4bb8dbda875b49358a655
adampridmore <320922+adampridmore@users.noreply.github.com>
2025-08-13 21:12:11 +0100

    Tidy up

--- Adam_Eddy_FSharp/Program.fs
+++ Adam_Eddy_FSharp/Program.fs
@@ -1,51 +1,10 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-
-// let x = ()
-// 
-// let a:  Option<string> = Some("HellO)")
-// let b: Option<string> = None
-// 
-
-// Option<String> a = Some("Hello")
-
-
-// Write a function Nil() which returns an empty list.
-
-  // type NIL = unit
-
-  // let myEmptyList : NIL = ()
-
-  // printfn "%O" myEmptyList
-
-
-// type C = C * C | unit
-// type E<'a> = Choice1 of 'a | Choice2 of 'a * 'a
-// type A = int * int
-// type B = {FirstName:string; LastName:string}
-
-// type B = {head: int; tail:B} | NIL
-// type B = int * (Value of B) | NIL
-// type A = {head: int; tail:B}
-
-
-// type Gift =
-// | Book of Book
-// | Chocolate of Chocolate
-// | Wrapped of Gift * WrappingPaperStyle
-// | Boxed of Gift
-// | WithACard of Gift * message:string
-// 
-
-// type A = | Item of head:int * A | Empty
-type ListOf<'T> = | Cons of head:'T * ListOf<'T> | Empty
+﻿type ListOf<'T> = | Cons of head:'T * ListOf<'T> | Empty
 
 let x =  Empty // An empty list 
 let y =  Cons(1, Empty) // Single item in list
 let z =  Cons(1, Cons(2, Empty)) // Two item in list
 
 printfn "x=%O" x
-
 printfn "y=%O" y
 printfn "z=%O" z
 
@@ -69,12 +28,6 @@ let tail<'T>(list: ListOf<'T>): ListOf<'T> =
   match list with
   | Cons( head:'T , tail) -> tail
   | Empty -> Empty
-
-//Cons of head:'T * ListOf<'T>
-// let head2<'T>(list: Cons) : 'T = 
-//   match list with
-//   | Cons( head:'T , _) -> Some(head)
-//   | _ -> None
 
 let sum(list: ListOf<int>): int = 
   let rec sumList(list: ListOf<int>, total: int): int = 
