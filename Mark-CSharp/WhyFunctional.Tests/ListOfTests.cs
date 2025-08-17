811e3b3b2d312ba03cd311e748d8a97c78aba4d0
Mark Allott <github.mark-allott@malsoft.com>
2025-08-18 00:06:55 +0100

    Tests to verify Exercise 1

---
+++ Mark-CSharp/WhyFunctional.Tests/ListOfTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+
+namespace WhyFunctional.Tests;
+
+[TestClass]
+public sealed class ListOfTests
+{
+	[TestMethod]
+	public void VerifyNilProducesEmptyResult()
+	{
+		ListOf<int> sut = ListOf<int>.Nil();
+
+		sut.Should().NotBeNull();
+		sut.ToArray.Should().BeEmpty();
+		sut.IsNil.Should().BeTrue();
+	}
+
+	[TestMethod]
+	public void VerifySingleElement()
+	{
+		ListOf<int> sut = ListOf<int>.Cons(1, ListOf<int>.Nil());
+
+		sut.Should().NotBeNull();
+		int[] values = sut.ToArray;
+		values.Should().NotBeEmpty();
+		values.Should().HaveCount(1);
+		values.Should().Contain(1);
+		sut.IsNil.Should().BeFalse();
+	}
+
+	[TestMethod]
+	public void VerifyThreeElements()
+	{
+		ListOf<int> sut = ListOf<int>.Cons(1, ListOf<int>.Cons(2, ListOf<int>.Cons(3, ListOf<int>.Nil())));
+
+		sut.Should().NotBeNull();
+		int[] values = sut.ToArray;
+		values.Should().NotBeEmpty();
+		values.Should().HaveCount(3);
+		values.Should().Contain([1, 2, 3]);
+		sut.IsNil.Should().BeFalse();
+	}
+}
