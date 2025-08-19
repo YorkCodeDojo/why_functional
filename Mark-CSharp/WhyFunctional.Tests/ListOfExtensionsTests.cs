cb8214f11f5ff045a32699db47112dafb6e33b59
Mark Allott <github.mark-allott@malsoft.com>
2025-08-19 09:13:07 +0100

    Updated to match requirements for Exercise 4

--- Mark-CSharp/WhyFunctional.Tests/ListOfExtensionsTests.cs
+++ Mark-CSharp/WhyFunctional.Tests/ListOfExtensionsTests.cs
@@ -22,4 +22,22 @@ public class ListOfExtensionsTests
 		var result = sut.Sum();
 		result.Should().Be(expected);
 	}
+	
+	[TestMethod]
+	[DataRow(new int[] { 1 })]
+	[DataRow(new int[] { 10 })]
+	[DataRow(new int[] { 1, 2, 3 })]
+	public void VerifyProductExtension(int[] values)
+	{
+		int expected = 1;
+		ListOf<int> sut = ListOf<int>.Nil();
+		foreach (int value in values.Reverse())
+		{
+			sut = ListOf<int>.Cons(value, sut);
+			expected *= value;
+		}
+
+		var result = sut.Product();
+		result.Should().Be(expected);
+	}
 }
--- Mark-CSharp/WhyFunctional/ListOfExtensions.cs
+++ Mark-CSharp/WhyFunctional/ListOfExtensions.cs
@@ -4,10 +4,29 @@ namespace WhyFunctional;
 
 public static class ListOfExtensions
 {
+	/// <summary>
+	/// Returns the summation of the values contained within <paramref name="list"/>
+	/// </summary>
+	/// <param name="list">A <seealso cref="ListOf{T}"/> values to be summed</param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns>The summation result of values held in <paramref name="list"/></returns>
 	public static T Sum<T>(this ListOf<T> list) where T : INumber<T>
 	{
 		return list.IsNil
 			? T.Zero
 			: list.Head + Sum(list.Tail);
+	}
+
+	/// <summary>
+	/// Returns the product of the values contained in <paramref name="list"/>
+	/// </summary>
+	/// <param name="list">A <seealso cref="ListOf{T}"/> values to be summed</param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns>The product of values held in <paramref name="list"/></returns>
+	public static T Product<T>(this ListOf<T> list) where T : INumber<T>
+	{
+		return list.IsNil
+			? T.One
+			: list.Head * Product(list.Tail);
 	}
 }
